e8fab83e02d09f9d7f486af11a77e0b133e581e7
CodingSexagenarian <104698521+CodingSexagenarian@users.noreply.github.com>
2022-11-26 16:48:32 -0500

    Completed Project.

---
+++ djangodelights/inventory/models.py
@@ -0,0 +1,98 @@
+from django.db import models
+
+UNIT_CHOICES = [
+    ("g", "gram"),
+    ("tbsp", "tablespoon"),
+    ("tsp", "teaspoon"),
+    ("l", "liter"),
+    ("ea", "each"),
+    ("cup", "cup"),
+    ("oz", "ounce"),
+    ("lbs", "pound"),
+    ("", ""),
+]
+
+# Create your models here.
+"""This model represents an ingredient that the restaurant has in 
+its inventory.
+"""
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Name", unique=True)
+    quantity = models.FloatField(default=0.0, verbose_name="Quantity")
+    unit = models.CharField(max_length=10, choices=UNIT_CHOICES, default="",
+                            verbose_name="Unit of Measure")
+    unit_price = models.FloatField(default=0.0, verbose_name="Unit Price")
+
+    def __str__(self):
+        return f"{self.name}: {self.unit_price} {self.unit}"
+
+    def get_absolute_url(self):
+        return "/ingredients"
+
+    class Meta:
+        ordering = ["name"]
+
+"""
+This model represents an item on the restaurant’s menu.
+"""
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=200, unique=True,
+                             verbose_name="Item Name")
+    price = models.FloatField(default=0.00, verbose_name="Price")
+    description = models.CharField(max_length=500, default="",
+                                   verbose_name="Item Description")
+    def __str__(self):
+        return f"name={self.name}; price={self.price}"
+
+    def get_absolute_url(self):
+        return "/menu"
+
+    def available(self):
+        return all(X.enough() for X in self.reciperequirement_set.all())
+
+    class Meta:
+        ordering = ["name"]
+
+"""
+This model represents a single ingredient and how much of it is required 
+for an item off the menu.
+"""
+
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE,
+                                  verbose_name="Menu Item")
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE,
+                                   verbose_name="Ingredient")
+    quantity = models.FloatField(default=0, verbose_name="Quantity")
+
+    def __str__(self):
+        return f"menu_item=[{self.menu_item.__str__()}]; " \
+               f"ingredient={self.ingredient.name}; qty={self.quantity}"
+
+    def get_absolute_url(self):
+        return "/menu"
+
+    def enough(self):
+        return self.quantity <= self.ingredient.quantity
+
+
+class Purchase(models.Model):
+
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE,
+                                  verbose_name="Menu Item")
+    timestamp = models.DateTimeField(auto_now_add=True,
+                                     verbose_name="DateTime")
+
+    def __str__(self):
+        return f"menu_item=[{self.menu_item.__str__()}]; time={self.timestamp}"
+
+    def get_absolute_url(self):
+        return "/purchases"
+
+    class Meta:
+        ordering = ["timestamp"]
---
+++ djangodelights/inventory/views.py
@@ -0,0 +1,149 @@
+from django.shortcuts import redirect
+from django_pandas.io import read_frame
+from django.db.models import Sum, F
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import logout
+from django.views.generic import TemplateView, ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.exceptions import SuspiciousOperation
+
+from .models import Ingredient, MenuItem, Purchase, RecipeRequirement
+from .forms import IngredientForm, MenuItemForm, RecipeRequirementForm
+
+from heapq import nlargest
+
+
+class HomeView(LoginRequiredMixin, TemplateView):
+    template_name = "inventory/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["ingredients"] = Ingredient.objects.all()
+        context["menu_items"] = MenuItem.objects.all()
+        context["purchases"] = Purchase.objects.all()
+        return context
+
+
+class IngredientsView(LoginRequiredMixin, ListView):
+    template_name = "inventory/ingredients_list.html"
+    model = Ingredient
+
+
+class NewIngredientView(LoginRequiredMixin, CreateView):
+    template_name = "inventory/add_ingredient.html"
+    model = Ingredient
+    form_class = IngredientForm
+
+
+class UpdateIngredientView(LoginRequiredMixin, UpdateView):
+    template_name = "inventory/update_ingredient.html"
+    model = Ingredient
+    form_class = IngredientForm
+
+
+class DeleteIngredientView(LoginRequiredMixin, DeleteView):
+    template_name = "inventory/delete_ingredient.html"
+    model = Ingredient
+    success_url = "/ingredients"
+
+
+class MenuView(LoginRequiredMixin, ListView):
+    template_name = "inventory/menu_list.html"
+    model = MenuItem
+
+
+class NewMenuItemView(LoginRequiredMixin, CreateView):
+    template_name = "inventory/add_menu_item.html"
+    model = MenuItem
+    form_class = MenuItemForm
+
+
+class UpdateMenuItemView(LoginRequiredMixin, UpdateView):
+    template_name = "inventory/update_menu_item.html"
+    model = MenuItem
+    form_class = MenuItemForm
+
+
+class DeleteMenuItemView(LoginRequiredMixin, DeleteView):
+    template_name = "inventory/delete_menu_item.html"
+    model = MenuItem
+    success_url = "/menu"
+
+
+class NewRecipeRequirementView(LoginRequiredMixin, CreateView):
+    template_name = "inventory/add_recipe_requirement.html"
+    model = RecipeRequirement
+    form_class = RecipeRequirementForm
+
+
+class PurchasesView(LoginRequiredMixin, ListView):
+    template_name = "inventory/purchase_list.html"
+    model = Purchase
+
+
+class NewPurchaseView(LoginRequiredMixin, TemplateView):
+    template_name = "inventory/add_purchase.html"
+    model = Purchase
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["menu_items"] = [X for X in MenuItem.objects.all() if X.available()]
+        return context
+
+    def post(self, request):
+        menu_item_id = request.POST["menu_item"]
+        menu_item = MenuItem.objects.get(pk=menu_item_id)
+        requirements = menu_item.reciperequirement_set
+        purchase = Purchase(menu_item=menu_item)
+
+        for requirement in requirements.all():
+            required_ingredient = requirement.ingredient
+            required_ingredient.quantity -= requirement.quantity
+            required_ingredient.save()
+
+        purchase.save()
+        return redirect("/purchases")
+
+
+class ReportView(LoginRequiredMixin, TemplateView):
+    template_name = "inventory/reports.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["purchases"] = Purchase.objects.all()
+        revenue = Purchase.objects.aggregate(
+            revenue=Sum("menu_item__price"))["revenue"]
+        total_cost = 0
+        for purchase in Purchase.objects.all():
+            for recipe_requirement in purchase.menu_item.reciperequirement_set.all():
+                total_cost += recipe_requirement.ingredient.unit_price * \
+                    recipe_requirement.quantity
+
+        purchases = Purchase.objects.values(
+            'timestamp',
+            item_name=F('menu_item__name'),
+            item_price=F('menu_item__price'),
+            item_desc=F('menu_item__description'))
+        df = read_frame(purchases)
+        grouped_purchases = df.groupby('item_name')[
+            'timestamp'].count().reset_index()
+        sort_purchases = grouped_purchases.sort_values("timestamp", axis=0,
+                                                       ascending=False,
+                                                       ignore_index=True)
+        top_3 = sort_purchases.nlargest(3, 'timestamp', keep='all')
+
+        list_of_top_3_purchases = top_3['item_name'].to_list()
+
+        context["revenue"] = revenue
+        context["total_cost"] = total_cost
+        context["profit"] = revenue - total_cost
+        context["gross_profit"] = (revenue - total_cost) / total_cost * 100
+        context["gross_margin_ratio"] = (revenue - total_cost) / revenue * 100
+        context["list_of_purchases"] = list_of_top_3_purchases
+
+        return context
+
+
+def log_out(request):
+    logout(request)
+    return redirect("/")
